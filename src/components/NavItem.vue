f110ecad7e209bc12d56f7d0128438cf03fe7389
Valeriy Petrenko <fourcher00@gmail.com>
2024-03-18 12:42:01 -0400

    NavItem refactoring

--- src/components/NavItem.vue
+++ src/components/NavItem.vue
@@ -1,14 +1,26 @@
-<script>
-export default {
-    inheritAttrs: false
-}
+<script setup>
+import { navigate, currentPage } from '@/router'
+import { isPageValid } from '@/validators'
+import { computed } from 'vue'
+
+const props = defineProps({
+  page: {
+    required: true,
+    type: String,
+    validator: isPageValid
+  }
+})
+
+const classes = computed(() => [
+  'flex flex-col items-center p-2 text-xs capitalize',
+  { 'pointer-event-none bg-gray-200': props.page === currentPage.value }
+])
 </script>
 
 <template>
-
-<li class="flex-1">
-    <a v-bind="$attrs" class="flex flex-col items-center p-2 text-xs capitalize" >
-        <slot></slot>
-    </a></li>
-
- </template>
+  <li class="flex-1">
+    <a :href="`#${page}`" :class="classes" @click="navigate(page)">
+      <slot></slot>
+    </a>
+  </li>
+</template>
--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -1,7 +1,6 @@
 <script setup>
 import NavItem from './NavItem.vue'
 import { NAV_ITEMS } from '@/constants'
-import { navigate, currentPage } from '@/router'
 </script>
 
 <template>
@@ -10,11 +9,8 @@ import { navigate, currentPage } from '@/router'
       <NavItem
         v-for="(icon, page) in NAV_ITEMS"
         :key="page"
-        :href="`#${page}`"
-        :class="{ 'bg-gray-200 pointer-event-none': page === currentPage }"
-        @click="navigate(page)"
-      >
-        <component :is="icon" class="h-6 w-6" /> {{ page }}
+        :page = "page">
+          <component :is="icon" class="h-6 w-6" /> {{ page }}
       </NavItem>
     </ul>
   </nav>
