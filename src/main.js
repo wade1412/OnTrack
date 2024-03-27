6056402f4207c8d921a694c9a1a4a6e8292c9fe3
Valeriy Petrenko <fourcher00@gmail.com>
2024-03-27 16:41:37 -0400

    Local storage logic refactored

--- src/main.js
+++ src/main.js
@@ -1,30 +1,13 @@
 import './assets/main.css'
 
-import * as Storage from './storage'
+import { loadState, saveState } from './storage'
 import App from './App.vue'
 import { createApp } from 'vue'
-import { timelineItems } from './timelineitems'
-import { activities } from './activities'
-import { isToday } from './time'
 
 loadState()
 
-document.addEventListener('visibilitychange', () => {
+document.addEventListener('visibilitychange', () =>
   document.visibilityState === 'visible' ? loadState() : saveState()
-})
-
-function saveState() {
-  Storage.save({
-    timelineItems: timelineItems.value,
-    activities: activities.value
-  })
-}
-
-function loadState() {
-  const state = Storage.load()
-
-  timelineItems.value = isToday(new Date(state.date)) ? state.timelineItems : timelineItems.value
-  activities.value = state.activities || activities.value
-}
+)
 
 createApp(App).mount('#app')
