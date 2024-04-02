0ad058efcea178626dc00b443fd626cc0093136a
Valeriy Petrenko <fourcher00@gmail.com>
2024-04-02 10:09:54 -0400

    Activities save states when date changes

--- src/storage.js
+++ src/storage.js
@@ -1,6 +1,6 @@
 import { APP_NAME } from './constants'
 import { today, isToday, endOfHour, toSeconds } from './time'
-import { activeTimelineItem, timelineItems } from './timelineitems'
+import { activeTimelineItem, timelineItems, resetTimelineItems } from './timelineitems'
 import { activities } from './activities'
 import { startTimelineItemTimer, stopTimelineItemTimer } from './timelineitem-timer'
 
@@ -23,9 +23,13 @@ export function loadState() {
 
   const lastActiveAt = new Date(state.lastActiveAt)
 
-  timelineItems.value = isToday(lastActiveAt)
-    ? syncIdleSeconds(state.timelineItems, lastActiveAt)
-    : timelineItems.value
+  timelineItems.value = state.timelineItems ?? timelineItems.value
+
+  if (activeTimelineItem.value && isToday(lastActiveAt)) {
+    timelineItems.value = syncIdleSeconds(state.timelineItems, lastActiveAt)
+  } else if (state.timelineItems && !isToday(lastActiveAt)) {
+    timelineItems.value = resetTimelineItems(state.timelineItems)
+  }
 }
 
 export function saveState() {
